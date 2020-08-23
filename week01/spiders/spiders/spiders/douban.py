99b03a02d01d46a639ad009d250d26d95183fc47
karen <451390409@qq.com>
2020-08-23 21:48:50 +0800

    替换bs的scrapy爬虫

--- week01/spiders/spiders/spiders/douban.py
+++ week01/spiders/spiders/spiders/douban.py
@@ -1,6 +1,7 @@
 import scrapy
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 from spiders.items import SpidersItem
+from scrapy.selector import Selector
 
 class DoubanSpider(scrapy.Spider):
     # 定义爬虫的名字
@@ -17,32 +18,45 @@ class DoubanSpider(scrapy.Spider):
     # start_requests()方法读取start_urls列表中的url并生成Request对象，发送给引擎
     # 引擎再指挥其他组件向网站服务器发送请求，下载网页
     def start_requests(self):
-        for i in range(0, 10):
+        for i in range(0, 1):
             url = f'https://movie.douban.com/top250?start={i*25}'
             # url 请求的网址
             # callback 回调函数，引擎会将下载好的页面（Response对象）发送给该方法进行数据解析
             # 这里可以使用callback指定新的函数，不是用parse作为默认的回调参数
             yield scrapy.Request(url=url, callback=self.parse)
+            # yield scrapy.Request(url=url, callback=self.parse, dont_filter=True)
 
     # 解析函数
     def parse(self, response):
-        items = []
-        soup = BeautifulSoup(response.text, 'html.parser')
-        title_list = soup.find_all('div', attrs="hd")
-        for tags in title_list:
+        # items = []
+        # soup = BeautifulSoup(response.text, 'html.parser')
+        # title_list = soup.find_all('div', attrs="hd")
+        # for tags in title_list:
+        #     item = SpidersItem()
+        #     for atags in tags.find_all("a"):
+        #         title = atags.find("span").text
+        #         link = atags.get("href")
+        #         item['title'] = title
+        #         item['link'] = link
+        #         items.append(item)
+        #         yield scrapy.Request(url=link, meta={'item':item}, callback=self.parse_details)
+
+        movies = Selector(response=response).xpath('//div[@class="hd"]')
+        for movie in movies:
             item = SpidersItem()
-            for atags in tags.find_all("a"):
-                title = atags.find("span").text
-                link = atags.get("href")
-                item['title'] = title
-                item['link'] = link
-                items.append(item)
-                yield scrapy.Request(url=link, meta={'item':item}, callback=self.parse_details)
+            title = movie.xpath('./a/span/text()')
+            link = movie.xpath('./a/@href')
+            item['title'] = title.extract_first()
+            item['link'] = link.extract_first()
+            yield scrapy.Request(url=link, meta={'item':item}, callback=self.parse_details)
+            
+            
 
     # 具体页面解析
     def parse_details(self, response):
         item = response.meta['item']
-        soup = BeautifulSoup(response.text, 'html.parser')
-        content = soup.find('div', attrs='related-info').get_text().strip()
+        # soup = BeautifulSoup(response.text, 'html.parser')
+        # content = soup.find('div', attrs='related-info').get_text().strip()
+        content = Selector(response=response).xpath('//div[@class="related-info"]')
         item['content'] = content
         yield item
