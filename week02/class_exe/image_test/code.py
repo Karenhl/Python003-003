a44afa7610f69eed3a8a11ad4bb1064020633265
karen <451390409@qq.com>
2020-08-30 21:41:00 +0800

    结构调整

--- week02/class_exe/image_test/code.py
+++ week02/class_exe/image_test/code.py
@@ -3,5 +3,27 @@ import os
 from PIL import Image
 import pytesseract
 
+# 打开并显示图片
 im = Image.open('timg.jpg')
 im.show()
+
+# 灰度处理图片
+gray = im.convert('L')
+gray.save('g_timg.jpg')
+im.close()
+
+# 二值化
+threshold = 100
+table = []
+
+for i in range(256):
+    if i < threshold:
+        table.append(0)
+    else:
+        table.append(1)
+
+out = gray.point(table, '1')
+out.save('b.jpg')
+
+th = Image.open('b.jpg')
+print(pytesseract.image_to_string(th, lang='chi_sim+eng'))
